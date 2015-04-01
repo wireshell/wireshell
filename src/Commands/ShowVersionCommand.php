750bffbc54357704e3c3ca5602db4d3fee1c9f02
mhe <mail@marcus-herrmann.com>
2015-04-01 15:33:48 +0200

    Add ShowVersionCommand

---
+++ src/Commands/ShowVersionCommand.php
@@ -0,0 +1,45 @@
+<?php namespace Wireshell\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Wireshell\PwConnector;
+
+/**
+ * Class ShowVersionCommand
+ *
+ * Returns the version of the current PW installation
+ *
+ * @package Wireshell
+ * @author Marcus Herrmann
+ */
+
+class ShowVersionCommand extends PwConnector
+{
+
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('show:version')
+            ->setAliases(['s:v'])
+            ->setDescription('Show version of current PW installation');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        
+        parent::bootstrapProcessWire($output);
+
+        $version = wire('config')->version;
+
+        $output->writeln("ProcessWire {$version}");
+
+    }
+}
