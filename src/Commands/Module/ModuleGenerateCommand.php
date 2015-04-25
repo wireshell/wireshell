f2185549eef35fd978fa7c4106bf10252fc478da
mhe <mail@marcus-herrmann.com>
2015-04-25 08:17:05 +0200

    Add command options and some defaults

--- src/Commands/Module/ModuleGenerateCommand.php
+++ src/Commands/Module/ModuleGenerateCommand.php
@@ -21,6 +21,17 @@ use Wireshell\PwConnector;
 class ModuleGenerateCommand extends PwConnector
 {
 
+    protected $defaults = [
+        'name' => 'My Module name',
+        'author' => 'Alexis Awesome',
+        'type' => 'Other',
+        'version' => 10,
+        'autoload' => 'false',
+        'permanent' => 'false',
+        'singular' => 'false'
+    ];
+
+
     /**
      * Configures the current command.
      */
@@ -29,8 +40,28 @@ class ModuleGenerateCommand extends PwConnector
         $this
             ->setName('module:generate')
             ->setAliases(['m:g'])
-            ->setDescription('Generates a module boilerplate')
-            ->addArgument('name', InputOption::VALUE_REQUIRED, 'Provide a class name for the module');
+            ->setDescription('Generates a boilerplate module')
+            ->addArgument('name', InputOption::VALUE_REQUIRED, 'Provide a class name for the module')
+            ->addOption('title', null, InputOption::VALUE_REQUIRED, 'Module title')
+            ->addOption('author', null, InputOption::VALUE_REQUIRED, 'Module author')
+            ->addOption('link', null, InputOption::VALUE_REQUIRED, 'Module link')
+            ->addOption('summary', null, InputOption::VALUE_REQUIRED, 'Module summary')
+            ->addOption('type', null, InputOption::VALUE_REQUIRED, 'Module type')
+            ->addOption('version', null, InputOption::VALUE_REQUIRED, 'Module version')
+            ->addOption('extends', null, InputOption::VALUE_REQUIRED, 'Module extends')
+            ->addOption('implements', null, InputOption::VALUE_REQUIRED, 'Module implements (Interface)')
+            ->addOption('permissions', null, InputOption::VALUE_REQUIRED, 'Module permissions')
+            ->addOption('require-pw', null, InputOption::VALUE_NONE, 'Module\'s ProcessWire version compatibility')
+            ->addOption('require-php', null, InputOption::VALUE_NONE, 'Module\'s PHP version compatibility')
+            ->addOption('is-autoload', null, InputOption::VALUE_NONE, 'autoload = true')
+            ->addOption('is-singular', null, InputOption::VALUE_NONE, 'singular = true')
+            ->addOption('is-permanent', null, InputOption::VALUE_NONE, 'permanent = true')
+            ->addOption('with-external-json', null, InputOption::VALUE_NONE, 'Generates external json config file')
+            ->addOption('with-copyright', null, InputOption::VALUE_NONE, 'Module author')
+            ->addOption('with-uninstall', null, InputOption::VALUE_NONE, 'Module author')
+            ->addOption('with-sample-code', null, InputOption::VALUE_NONE, 'Module author')
+            ->addOption('with-config-page', null, InputOption::VALUE_NONE, 'Module author');
+
     }
 
     /**
