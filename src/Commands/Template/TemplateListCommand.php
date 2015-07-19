b2bc84006748cffd6a35042ab663334e428aefb3
Tabea David <info@justonestep.de>
2015-07-19 23:21:57 +0200

    adding templates list command

---
+++ src/Commands/Template/TemplateListCommand.php
@@ -0,0 +1,75 @@
+<?php namespace Wireshell\Commands\Template;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Wireshell\Helpers\PwConnector;
+use Wireshell\Helpers\WsTables as WsTables;
+
+/**
+ * Class TemplateListCommand
+ *
+ * Creating ProcessWire templates
+ *
+ * @package Wireshell
+ * @author Tabea David
+ */
+class TemplateListCommand extends PwConnector
+{
+
+    /**
+     * Configures the current command.
+     */
+    public function configure()
+    {
+        $this
+            ->setName('template:list')
+            ->setAliases(['t:l'])
+            ->setDescription('Lists ProcessWire templates')
+            ->addOption('advanced', null, InputOption::VALUE_NONE, 'Show system templates. By default, system/internal templates are not shown.');
+          ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::bootstrapProcessWire($output);
+
+        $advanced = $input->getOption('advanced') ? true : false;
+
+        $content = $this->getTemplateData($advanced);
+        $headers = array('Template', 'Fields', 'Pages', 'Modified', 'Access');
+        $tables = array(WsTables::buildTable($output, $content, $headers));
+        WsTables::renderTables($output, $tables);
+    }
+
+    /**
+     * get templates data
+     *
+     * @param boolean $advanced
+     * @return array
+     */
+    private function getTemplateData($advanced) {
+        $content = array();
+        $advanced = wire('config')->advanced || $advanced;
+        foreach(wire('templates') as $t) {
+            if (!$advanced && ($t->flags & \Template::flagSystem)) continue;
+
+            $content[] = array(
+                $t->name,
+                count($t->fieldgroup),
+                $t->getNumPages(),
+                wireRelativeTimeStr($t->modified),
+                $t->flags & \Template::flagSystem ? '✖' : ''
+            );
+        }
+
+        return $content;
+    }
+
+}
